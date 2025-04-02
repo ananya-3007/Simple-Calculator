b7de1e166d4719202375943f9f8cedde62ed3db6
Ananya N <145993819+ananya-3007@users.noreply.github.com>
2025-04-02 14:06:58 +0530

    Add files via upload
    A Simple Calculator which Adds , Subtracts , Multiplies , Divides the provided 2 numbers

---
+++ simple_calculator.py
@@ -0,0 +1,10 @@
+#Simple-Calculator
+#A simple calculator used to make operations like Addition,Subtraction,Multiplication and Division by just passing 2 numbers.
+
+
+a=int(input('Enter the first number:'))            #Passing first number
+b=int(input('Enter the second number:'))           #Passing second number
+print('The sum is:',a+b)                           #Adds the 2 numbers
+print('The difference is:',a-b)                    #Subtracts the 2 numbers
+print('The product is:',a*b)                       #Multiplies the 2 numbers
+print('The quotient is:',a/b)                      #Divides the 2 numbers
